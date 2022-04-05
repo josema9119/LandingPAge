fbd5fb442c1adf2629d70240cb013cdb394ffd4c
josema9119 <jose.ma.mm.91@gmail.com>
2022-04-05 19:21:15 +0000

    last change

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -5,10 +5,12 @@ const Jumbotron = () => {
 		<div className="container-fluid col-8 py-5 bg-light rounded-3 ">
 			<h1 className="display-5 fw-bold pl-2">A Warm Welcome!</h1>
 			<p className="fs-4 p-2">
-				Using a series of utilities, you can create this jumbotron, just
-				like the one in previous versions of Bootstrap. Check out the
-				examples below for how you can remix and restyle it to your
-				liking
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industry's standard dummy
+				text ever since the 1500s, when an unknown printer took a galley
+				of type and scrambled it to make a type specimen book. It has
+				survived not only five centuries, but also the leap into
+				electronic typesetting, remaining essentially unchanged.
 			</p>
 			<button className="btn btn-primary btn-lg" type="button">
 				Call to action!
